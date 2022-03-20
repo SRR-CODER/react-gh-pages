5c08e38873a6fdff0bf697aa9509c035ca0c5e6f
SAkET ROsHAn RAi <78961362+SRR-CODER@users.noreply.github.com>
2022-03-20 16:58:25 +0530

    Updates

--- src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Heading(){
-    return(
-        <div >
-            <h1 className="heading">The people behind Wild<span className="beats">Beats</span></h1>
-        </div>
-    )
-}
